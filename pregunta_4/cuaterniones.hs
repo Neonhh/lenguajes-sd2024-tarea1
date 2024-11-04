76f015806e3dfb90f6586be0c6d7c4f9e4fa8bdd
Neonhh <thenjhu@gmail.com>
2024-11-04 14:20:54 -0400

    ajustes a cuaterniones.hs (post-entrega)

--- pregunta_4/cuaterniones.hs
+++ pregunta_4/cuaterniones.hs
@@ -1,35 +1,41 @@
 --Definimos el cuaternion y los elementos a exportar del mismo
---exportaremos el tipo de datos junto con los operadores
-module Cuaterniones (Cuaternion(..), (+), (~), (*), (&)) where
+newtype Cuaternion = Cuaternion (Float, Float, Float, Float) deriving Eq
 
--- Ocultamos los operadores conflictivos del Prelude
-import Prelude hiding ((+), (*), (&))
+instance Show Cuaternion where
+    show :: Cuaternion -> String
+    show (Cuaternion (a, b, c, d)) = show a ++ " + " ++ show b ++ "i + " ++ show c ++ "j + " ++ show d ++ "k"
 
---el tipo de datos consiste en 4 Floats, que representan los elementos del cuaternion (a, bi, cj, dk)
---y pueden ser mostrados por pantalla (Show) y comparados por igualdad (Eq)
-data Cuaternion = Cuaternion Float Float Float Float deriving (Eq, Show)
+instance Num Cuaternion where
+    --suma
+    (+) :: Cuaternion -> Cuaternion -> Cuaternion
+    (Cuaternion (a1, b1, c1, d1)) + (Cuaternion (a2, b2, c2, d2)) = Cuaternion (a1 + a2, b1 + b2, c1 + c2, d1 + d2)
 
---definimos los operadores
+    --negado
+    negate :: Cuaternion -> Cuaternion
+    negate (Cuaternion (a, b, c, d)) = Cuaternion (-a, -b, -c, -d)
 
---suma
-infixl 6 +
-(+) :: Cuaternion -> Cuaternion -> Cuaternion
-(Cuaternion a1 b1 c1 d1) + (Cuaternion a2 b2 c2 d2) = Cuaternion (a1 + a2) (b1 + b2) (c1 + c2) (d1 + d2)
+    --producto
+    (*) :: Cuaternion -> Cuaternion -> Cuaternion
+    (Cuaternion (a1, b1, c1, d1)) * (Cuaternion (a2, b2, c2, d2)) = Cuaternion (a1*a2 - b1*b2 - c1*c2 - d1*d2, a1*b2 + b1*a2 + c1*d2 - d1*c2, a1*c2 - b1*d2 + c1*a2 + d1*b2, a1*d2 + b1*c2 - c1*b2 + d1*a2)
 
---conjugada
-(~) :: Cuaternion -> Cuaternion 
-(~) (Cuaternion a b c d) = Cuaternion a (-b) (-c) (-d)
+    --norma o valor absoluto
+    abs :: Cuaternion -> Cuaternion
+    abs (Cuaternion (a, b, c, d)) = Cuaternion (sqrt (a^2 + b^2 + c^2 + d^2), 0, 0, 0)
 
---producto
-infixl 7 *
-(*) :: Cuaternion -> Cuaternion -> Cuaternion
-(Cuaternion a1 b1 c1 d1) * (Cuaternion a2 b2 c2 d2) = Cuaternion (a1*a2 - b1*b2 - c1*c2 - d1*d2) (a1*b2 + b1*a2 + c1*d2 - d1*c2) (a1*c2 - b1*d2 + c1*a2 + d1*b2) (a1*d2 + b1*c2 - c1*b2 + d1*a2)
+    -- resto de definiciones  - signum y fromInteger
+    signum :: Cuaternion -> Cuaternion
+    signum (Cuaternion (a, b, c, d)) = Cuaternion (a / norm, b / norm, c / norm, d / norm)
+        where 
+            norm :: Float
+            norm = Main.and (Cuaternion (a, b, c, d))^2
+    
+    fromInteger :: Integer -> Cuaternion
+    fromInteger a = Cuaternion (fromInteger a, 0, 0, 0)
 
--- norma (este operador councide con el que se una para and bit a bit)
-(&) :: Cuaternion -> Float
-(&) (Cuaternion a b c d) = sqrt (a^2 + b^2 + c^2 + d^2)
+-- valor absoluto como flotante
+and :: Cuaternion -> Float
+and (Cuaternion (a, b, c, d)) = sqrt (a^2 + b^2 + c^2 + d^2)
 
---definicion de las operaciones con numeros enteros o flotantes
-    -- No alcance a esto :,)
-
-
+-- conjugada
+con :: Cuaternion -> Cuaternion
+con (Cuaternion (a, b, c, d)) = Cuaternion (a, -b, -c, -d)
